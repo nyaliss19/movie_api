02a44f7623c4d5501a13ccce437b268c50c47ecc
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-05-02 21:49:36 -0400

    Update index.js
    Created object with favorite movies

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 const express = require('express');
 const app = express();
+
+let topMovies = [
+    {
+        title: 'The Matrix',
+        year: '1999'
+    },
+    {
+        title: 'Awakenings',
+        year: '1990'
+    },
+    {
+        title: 'Pans Labyrinth',
+        year: '2006'
+    },
+    {
+        title: 'Chronicles of Riddick',
+        year: '2004'
+    },
+    {
+        title: 'Lady and the Tramp',
+        year: '1955'
+    },
+    {
+        title: 'Annihilation',
+        year: '2018'
+    },
+    {
+        title: 'Arrival',
+        year: '2016'
+    },
+    {
+        title: 'The Matrix Resurrections',
+        year: '2021'
+    },
+    {
+        title: 'Speed',
+        year: '1994'
+    },
+    {
+        title: 'Twin Peaks: Fire Walk With Me',
+        year: '1992'
+    },
+];
+
