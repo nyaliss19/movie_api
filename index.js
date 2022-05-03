843461824a2cd74db059c0ee083b1e879d4ce0b8
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-05-02 22:17:11 -0400

    Update index.js
    added morgan functionality

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan'),
+    fs = require('fs'),
+    path = require('path');
+
 const app = express();
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+
+app.use(morgan('combined', {stream: accessLogStream}));
 
 let topMovies = [
     {
