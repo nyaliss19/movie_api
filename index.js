bd466c286c068e2976b4d8e1a879d281f658b4fc
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-06-28 19:49:40 -0400

    Deleted old code
    Deleted old code

--- index.js
+++ index.js
@@ -126,14 +126,6 @@ app.post('/users', (req, res) => {
     });
 });
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send('users need names')
-    }
-})
 //get all users
 app.get('/users', (req, res) => {
     Users.find()
