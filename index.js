97aab3429f97a20dce594a872463e78e598c12ec
nyaliss19 <53007681+nyaliss19@users.noreply.github.com>
2022-08-16 18:23:42 -0400

    Added a space for cleaner presentation

--- index.js
+++ index.js
@@ -225,9 +225,9 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
     Users.findOneAndRemove({ Username: req.params.Username })
     .then((user) => {
         if (!user) {
-            res.status(400).send(req.params.Username + 'was not found.');
+            res.status(400).send(req.params.Username + ' was not found.');
         } else {
-            res.status(200).send(req.params.Username + 'was deleted.');
+            res.status(200).send(req.params.Username + ' was deleted.');
         }
     })
     .catch((err) => {
